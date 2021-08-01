1db8603738a75a162832d0c73a1965857e9cb81c
carbaj03 <carbaj03@gmail.com>
2021-08-01 06:58:43 +0200

    Add Async validation
    It's impossible show a loading state and then the result.
    
    Since side effects are not allowed inside of reducer, i have to create an extra step before i call the reducer, in order to execute the side effects.

--- app/src/main/java/com/acv/reduxessentials/MainActivity.kt
+++ app/src/main/java/com/acv/reduxessentials/MainActivity.kt
@@ -13,11 +13,24 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.lifecycleScope
 import com.acv.reduxessentials.ui.theme.ReduxEssentialsTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    private val store = Store(AppState(ATM(0)))
+    private val store = Store(
+        initialState = AppState(
+            atm = ATM(0),
+            error = false,
+            loading = false
+        ),
+        coroutineContext = lifecycleScope.coroutineContext
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,33 +48,57 @@ class MainActivity : ComponentActivity() {
 }
 
 data class AppState(
-    val atm: ATM
+    val atm: ATM,
+    val error: Boolean,
+    val loading: Boolean,
 )
 
 sealed interface Action {
     data class Deposit(val amount: String) : Action
     data class Withdrawal(val amount: String) : Action
+    object Validating : Action
+    object DepositSucceed : Action
+    object DepositError : Action
+    object WithdrawalSucceed : Action
+    object WithdrawalError : Action
 }
 
 class Store(
-    initialState: AppState
-) {
+    initialState: AppState,
+    override val coroutineContext: CoroutineContext
+) : CoroutineScope {
     var state = MutableStateFlow(initialState)
 
     fun dispatch(action: Action) {
-        state.value = state.value.reduce(action)
+        launch { state.value = state.value.reduce(action) }
     }
 
-    private fun AppState.reduce(action: Action): AppState =
+    private suspend fun AppState.reduce(action: Action): AppState =
         when (action) {
-            is Action.Deposit -> state.value.copy(state.value.atm.copy(state.value.atm.balance + action.amount.validate()))
-            is Action.Withdrawal -> state.value.copy(state.value.atm.copy(state.value.atm.balance - action.amount.validate()))
+            is Action.Deposit -> {
+                if (validateTransaction()) state.value.copy(state.value.atm.copy(state.value.atm.balance + action.amount.validate()))
+                else this
+            }
+            is Action.Withdrawal -> {
+                if (validateTransaction()) state.value.copy(state.value.atm.copy(state.value.atm.balance - action.amount.validate()))
+                else this
+            }
+            is Action.Validating -> state.value.copy(loading = true)
+            is Action.DepositError -> state.value.copy(error = true, loading = false)
+            is Action.DepositSucceed -> state.value.copy(error = false, loading = false)
+            is Action.WithdrawalError -> state.value.copy(error = true, loading = false)
+            is Action.WithdrawalSucceed -> state.value.copy(error = false, loading = false)
         }
 }
 
 data class ATM(
     val balance: Int,
 )
+
+suspend fun validateTransaction(): Boolean {
+    delay(8000)
+    return Random.nextBoolean()
+}
 
 @Composable
 private fun App(store: Store) {
@@ -81,19 +118,29 @@ private fun App(store: Store) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = state.atm.balance.toString())
-
-        TextField(
-            value = amount,
-            onValueChange = { amount = it },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
-        Row {
-            Button(modifier = Modifier.padding(8.dp), onClick = deposit) {
-                Text(text = "Deposit")
+        when {
+            state.error -> {
+                Text("Error")
             }
-            Button(modifier = Modifier.padding(8.dp), onClick = withdraw) {
-                Text(text = "Withdraw")
+            state.loading -> {
+                Text(text = "Loading...")
+            }
+            else -> {
+                Text(text = state.atm.balance.toString())
+
+                TextField(
+                    value = amount,
+                    onValueChange = { amount = it },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+                )
+                Row {
+                    Button(modifier = Modifier.padding(8.dp), onClick = deposit) {
+                        Text(text = "Deposit")
+                    }
+                    Button(modifier = Modifier.padding(8.dp), onClick = withdraw) {
+                        Text(text = "Withdraw")
+                    }
+                }
             }
         }
     }
